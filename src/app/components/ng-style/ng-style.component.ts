5abc43e4131285f7b31d969a244c4a3bfb2ec61a
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-05-09 22:01:39 +0200

    ng style done

--- src/app/components/ng-style/ng-style.component.ts
+++ src/app/components/ng-style/ng-style.component.ts
@@ -4,13 +4,39 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-ng-style',
   template: `
   <h2>NgStyle <small><a href="https://angular.io/api/common/NgStyle">docs</a></small></h2>
-    <p [ngStyle]="{">
+
+
+
+    <p [style.fontSize.px]="size">
       Hello world... this is a tag
     </p>
+
+    <button class="btn btn-primary" (click)="size = size + 5">
+    <i class="fa fa-plus"></i>
+    </button>
+    
+    <button class="btn btn-primary" (click)="size = size - 5">
+    <i class="fa fa-minus"></i>
+    </button><br><br>
+    <textarea  rows="10" cols="70" disabled>    size = 10; 
+    <p [style.fontSize.px]="size">
+      Hello world... this is a tag
+    </p>
+  
+    <button class="btn btn-primary" (click)="size = size + 5">
+      <i class="fa fa-plus"></i>
+    </button>
+    
+    <button class="btn btn-primary" (click)="size = size - 5">
+      <i class="fa fa-minus"></i>
+    </button> </textarea>
   `,
   styles: []
 })
 export class NgStyleComponent implements OnInit {
+
+  size = 10;
+
 
   constructor() { }
 
