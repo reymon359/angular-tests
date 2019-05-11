cf664710b3cd9d78935397fa9566c03151760420
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-05-11 20:05:06 +0200

    angular directives

--- src/app/directives/highlighted.directive.ts
+++ src/app/directives/highlighted.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlighted]'
@@ -6,7 +6,21 @@ import { Directive, ElementRef } from '@angular/core';
 export class HighlightedDirective {
 
   constructor(private elementRef: ElementRef) {
-  elementRef.nativeElement.style.backgroundColor =  "yellow";
-   }
+    // elementRef.nativeElement.style.backgroundColor =  "yellow";
+  }
 
+  @Input('appHighlighted') newColor: string;
+
+  @HostListener('mouseenter') mouseEntered() {
+    // this.elementRef.nativeElement.style.backgroundColor = "yellow";
+    this.highlight(this.newColor || 'yellow');
+  }
+  @HostListener('mouseleave') mouseLeft() {
+    this.highlight(null);
+
+  }
+  private highlight(color: string) {
+    this.elementRef.nativeElement.style.backgroundColor = color;
+
+  }
 }
