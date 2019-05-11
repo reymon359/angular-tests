c32f67bf276a80bc2f545c66d466d7a8b1c4a759
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-05-11 19:12:17 +0200

    async process

--- src/app/components/classes/classes.component.ts
+++ src/app/components/classes/classes.component.ts
@@ -9,12 +9,19 @@ export class ClassesComponent implements OnInit {
   alert: string = "alert-danger";
 
   properties: Object = {
-  danger:true
+    danger: true
   };
+
+  loading: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  executeProcess() {
+    this.loading = true;
+    setTimeout(() => this.loading = false, 2500);
   }
 
 }
